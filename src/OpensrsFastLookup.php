5d937b60470467632d9fa0ec2492f1427c58cf5e
James Wade <jpswade@gmail.com>
2019-03-25 21:07:04 +0000

    feat: add classes

---
+++ src/OpensrsFastLookup.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\DeadUseful;
+
+use Exception;
+
+class OpensrsFastLookup
+{
+    /**
+     * OpenSRS domain service API url.
+     * LIVE => rr-n1-tor.opensrs.net, TEST => horizon.opensrs.net
+     */
+    const OSRS_HOST = 'rr-n1-tor.opensrs.net';
+
+    /**
+     * OpenSRS API fastlookup port.
+     */
+    const OSRS_FASTLOOKUP_PORT = 51000;
+
+    /**
+     * @var array
+     */
+    private $result = [];
+
+    /**
+     * @var string
+     */
+    private $host = self::OSRS_HOST;
+
+    /**
+     * @var string
+     */
+    private $port = self::OSRS_FASTLOOKUP_PORT;
+
+    /**
+     * OpensrsFastLookup constructor.
+     * @param $query string The domain to query.
+     */
+    function __construct($query)
+    {
+        $this->checkDomain($query);
+    }
+
+    /**
+     * @param $query
+     * @return void
+     * @throws Exception
+     */
+    public function checkDomain($query)
+    {
+        $command = "check_domain $query" . PHP_EOL;
+        $response = self::lookup($command, $this->getHost(), $this->getPort());
+        $results = explode(' ', $response, 2);
+        $responseCode = trim($results[0]);
+        if (empty($responseCode)) {
+            throw new Exception('Empty response.');
+        }
+        $responseCodes = [
+            210 => 'available',
+            211 => 'taken',
+            465 => 'invalid_domain',
+            5050 => 'invalid_command',
+            555 => 'invalid_ip',
+            701 => 'unknown_tld',
+        ];
+        if (array_key_exists($responseCode, $responseCodes) === false) {
+            throw new Exception('Unexpected response: ' . $response);
+        }
+        $result = [
+            'response' => $response,
+            'code' => $responseCode,
+            'status' => $responseCodes[$responseCode]
+        ];
+        $this->setResult($result);
+    }
+
+    /**
+     * @param string $payload
+     * @param string $host
+     * @param int $port
+     * @param int $length
+     * @param int $timeout
+     * @return string
+     */
+    public function lookup($payload, $host = self::OSRS_HOST, $port = self::OSRS_FASTLOOKUP_PORT, $length = 2048, $timeout = 1)
+    {
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $options = ['sec' => $timeout, 'usec' => 0];
+        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, $options);
+        socket_connect($socket, $host, $port);
+        socket_send($socket, $payload, strlen($payload), 0);
+        $out = socket_read($socket, $length);
+        socket_close($socket);
+        return $out;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     * @return OpensrsFastLookup
+     */
+    public function setHost(string $host): OpensrsFastLookup
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPort(): string
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param string $port
+     * @return OpensrsFastLookup
+     */
+    public function setPort(string $port): OpensrsFastLookup
+    {
+        $this->port = $port;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    /**
+     * @param array $result
+     */
+    public function setResult($result)
+    {
+        $this->result = $result;
+    }
+}
---
+++ src/OpensrsLookup.php
@@ -0,0 +1,304 @@
+<?php
+
+namespace App\DeadUseful;
+
+use Exception;
+use SimpleXMLElement;
+
+/**
+ * OpenSRS reseller username.
+ */
+define('OSRS_USERNAME', getenv('OSRS_USERNAME'));
+
+/**
+ * OpenSRS reseller private Key. Please generate a key if you do not already have one.
+ */
+define('OSRS_KEY', getenv('OSRS_KEY'));
+
+class OpensrsLookup
+{
+    /**
+     * OpenSRS domain service API url.
+     * LIVE => rr-n1-tor.opensrs.net, TEST => horizon.opensrs.net
+     */
+    const OSRS_HOST = 'https://rr-n1-tor.opensrs.net:55443';
+
+    /**
+     * Socket Timeout in seconds.
+     */
+    const SOCKET_TIMEOUT = 120;
+
+    /**
+     * @var string
+     */
+    private $action = '';
+
+    /**
+     * @var string
+     */
+    private $query = '';
+
+    /**
+     * @var array
+     */
+    private $result = [];
+
+    /**
+     * @var int
+     */
+    private $timeout = self::SOCKET_TIMEOUT;
+
+    /**
+     * @var string
+     */
+    private $host = self::OSRS_HOST;
+
+    /**
+     * @var string
+     */
+    private $username = OSRS_USERNAME;
+
+    /**
+     * @var string
+     */
+    private $key = OSRS_KEY;
+
+    /**
+     * DomainTransferable constructor.
+     * @param $query
+     * @param string $action
+     * @param string $username
+     * @param string $key
+     */
+    public function __construct($query, $action = 'lookup', $username = OSRS_USERNAME, $key = OSRS_KEY)
+    {
+        $this->setQuery($query);
+        $this->setAction($action);
+        $this->setUsername($username);
+        $this->setKey($key);
+        $this->process();
+    }
+
+    /**
+     * Process query.
+     * @throws Exception
+     */
+    private function process()
+    {
+        $request = $this->encode();
+        $headers = $this->buildHeaders($request);
+        $result = $this->filePostContents($this->getHost(), $request, $headers);
+        if (empty($result)) {
+            throw new Exception('Empty response.');
+        }
+        $xml = simplexml_load_string($result, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if (is_object($xml) === false) {
+            throw new Exception('Unable to load XML response.');
+        }
+        $dataBlock = [];
+        foreach ($xml->body->data_block->dt_assoc->item as $item) {
+            $key = (string)$item->attributes()['key'];
+            $value = $item;
+            $dataBlock[$key] = $value;
+        }
+        $responseCode = (int)$dataBlock['response_code'];
+        if ($responseCode === 401) {
+            throw new Exception('Username or key is incorrect, please check your config file.');
+        }
+        if ($responseCode > 299) {
+            throw new Exception($dataBlock['response_text']);
+        }
+        $attributes = [];
+        foreach ($dataBlock['attributes']->dt_assoc->item as $item) {
+            $key = (string)$item->attributes()['key'];
+            $value = (string)$item;
+            $attributes[$key] = $value;
+        }
+        $this->setResult($attributes);
+    }
+
+    /**
+     * Converts a PHP array into an OPS message.
+     * @return string OPS XML message.
+     */
+    function encode()
+    {
+        $xml = new SimpleXMLElement('<!DOCTYPE OPS_envelope SYSTEM "ops.dtd"><OPS_envelope></OPS_envelope>');
+        $assoc = $xml->addChild('body')->addChild('data_block')->addChild('dt_assoc');
+        $assoc->addChild('item', 'XCP')->addAttribute('key', 'protocol');
+        $assoc->addChild('item', $this->getAction())->addAttribute('key', 'action');
+        $assoc->addChild('item', 'DOMAIN')->addAttribute('key', 'object');
+        $attributes = $assoc->addChild('item');
+        $attributesAssoc = $attributes->addChild('dt_assoc');
+        $attributesAssoc->addChild('item', $this->getQuery())->addAttribute('key', 'domain');
+        $attributes->addAttribute('key', 'attributes');
+        $request = $xml->asXML();
+        return $request;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param string $action
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * @param string $query
+     */
+    public function setQuery($query)
+    {
+        $this->query = $query;
+    }
+
+    /**
+     * Builds the headers.
+     *
+     * @param $request
+     * @return string
+     */
+    private function buildHeaders($request)
+    {
+        $len = strlen($request);
+        $signature = md5(md5($request . $this->getKey()) . $this->getKey());
+        $header[] = 'Content-Type: text/xml';
+        $header[] = 'X-Username: ' . $this->getUsername();
+        $header[] = 'X-Signature: ' . $signature;
+        $header[] = 'Content-Length: ' . $len;
+        $headers = implode(PHP_EOL, $header);
+        return $headers;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     * @return OpensrsLookup
+     */
+    public function setKey(string $key): OpensrsLookup
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return OpensrsLookup
+     */
+    public function setUsername(string $username): OpensrsLookup
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * Similar to file_get_contents but uses the POST method.
+     *
+     * @param $host
+     * @param $content
+     * @param $headers
+     * @return string
+     * @throws Exception
+     */
+    private function filePostContents($host, $content, $headers)
+    {
+        $opts = [
+            'http' =>
+                [
+                    'method' => 'POST',
+                    'header' => $headers,
+                    'content' => $content,
+                    'timeout' => $this->getTimeout()
+                ]
+        ];
+        $context = stream_context_create($opts);
+        $flags = null;
+        $contents = @file_get_contents($host, $flags, $context);
+        return $contents;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeout(): int
+    {
+        return $this->timeout;
+    }
+
+    /**
+     * @param int $timeout
+     * @return OpensrsLookup
+     */
+    public function setTimeout(int $timeout): OpensrsLookup
+    {
+        $this->timeout = $timeout;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     * @return OpensrsLookup
+     */
+    public function setHost(string $host): OpensrsLookup
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    /**
+     * @param array $result
+     * @return OpensrsLookup
+     */
+    public function setResult($result)
+    {
+        $this->result = $result;
+        return $this;
+    }
+}
