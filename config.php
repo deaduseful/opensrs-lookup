fd0173119b9c790c94641ee3301a70f594ba5ee0
James Wade <jpswade@gmail.com>
2020-05-19 21:14:19 +0100

    fix: don't import empty lines

--- config.php
+++ config.php
@@ -4,6 +4,8 @@ $file = __DIR__ . '/.env';
 if (file_exists($file)) {
     $env = file($file, FILE_IGNORE_NEW_LINES);
     foreach ($env as $line) {
-        putenv($line);
+        if ($line) {
+            putenv($line);
+        }
     }
 }
