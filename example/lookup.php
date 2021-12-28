e7b9e780d303baaec889c4ff4e44a6751d693444
James Wade <jpswade@gmail.com>
2021-12-28 20:48:52 +0000

    fix: add lookup example

---
+++ example/lookup.php
@@ -0,0 +1,17 @@
+<?php
+
+use Deaduseful\Opensrs\Lookup;
+
+require_once '../vendor/autoload.php';
+require_once '../config.php';
+
+$query = $argv[1] ?? 'example.com';
+
+try {
+    $lookup = new Lookup();
+    $result = $lookup->getDomain($query);
+    var_dump($result);
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+}
+echo PHP_EOL;
