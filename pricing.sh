bed731e0f2192327cac193f782cc31d4ae8ccf42
James Wade <jpswade@gmail.com>
2022-08-25 16:16:53 +0100

    fix: add shell command for testing

---
+++ pricing.sh
@@ -0,0 +1,2 @@
+curl 'https://opensrs.com/wp-admin/admin-ajax.php' \
+  --data-raw 'action=angus_get_dataset&data=eyJvcHRpb25LZXkiOiJhbmd1c19zeW5jXzBhNmNiNzJkLTE3ZjMtNDFhZC05Y2E5LWQ1MjM1NzliM2U4MSIsInRsZHMiOmZhbHNlfQ'>pricing.json
